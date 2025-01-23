821924787f00f20dbbbc252e32640d52cab77f6a
Pedro Fonseca <pedrofonunes@gmail.com>
2025-01-22 21:10:00 -0300

    feat: add project in github

---
+++ src/main/java/com/pedrofonunes/database/account/DataBaseAccountApplication.java
@@ -0,0 +1,13 @@
+package com.pedrofonunes.database.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrofonunes/database/account/DataBaseAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrofonunes.database.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
